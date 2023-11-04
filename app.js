01759a96e8ebb20a09c1e54fe84f9a595448f14e
Junaid Kaliyadan <78746795+j22k@users.noreply.github.com>
2023-11-04 07:45:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -58,32 +58,6 @@ var app = express();
 const multer = require('multer');
 const upload = multer(); // Initialize multer
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(bodyParser.json());
-app.use(upload.none());
-
-app.use(session({ secret: "key", cookie: { maxAge: 3600000 } }));
-db.connectToDatabase()
-// Define routes
-app.use('/', indexRouter);
-// Define other routes using app.use, e.g., app.use('/admin', adminRouter);
-
-// Catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// Error handler
-app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error'); // Make sure you have an error view (e.g., error.hbs) in your 'views' directory.
-});
-
 // Start the server
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
@@ -115,3 +89,29 @@ function onListening() {
   // ...
 }
 
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(upload.none());
+
+app.use(session({ secret: "key", cookie: { maxAge: 3600000 } }));
+db.connectToDatabase()
+// Define routes
+app.use('/', indexRouter);
+// Define other routes using app.use, e.g., app.use('/admin', adminRouter);
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// Error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error'); // Make sure you have an error view (e.g., error.hbs) in your 'views' directory.
+});
